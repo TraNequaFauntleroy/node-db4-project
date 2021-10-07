a9213edc85682589df9702f093866892c7792466
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-07 16:52:11 -0400

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,4 @@ const knex = require('knex')
 const configurations = require('../knexfile')
 const environment = process.env.NODE_ENV
 
-module.exports = knex(configurations)
+module.exports = knex(configurations[environment])
