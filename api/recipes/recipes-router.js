311ae69a6e8e4237e529fdb81f4ba534455b4c63
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-07 16:35:43 -0400

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -12,7 +12,8 @@ router.get('/:recipe_id', (req, res, next) => {
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(500).json({
         customMessage: 'something went wrong inside the recipes router',
-        message: err.message
+        message: err.message, 
+        stack:err
     })
 })
 
