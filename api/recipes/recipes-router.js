bf04fd5e37f9723f9ee46afff737480c6a2b5f36
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-07 15:43:12 -0400

    GET/:id EP fleshed out

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,4 +1,13 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
+
+router.get('/recipe:id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.id)
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(next)
+})
 
 router.use('*', (req, res) => {
     res.json({ api: 'up' })
